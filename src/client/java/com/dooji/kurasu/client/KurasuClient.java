0b7243cfec4d8ba09682f53cea62d62f108c22fa
dooji2 <doojisbasement@gmail.com>
2026-05-01 19:00:59 +0300

    add template code

---
+++ src/client/java/com/dooji/kurasu/client/KurasuClient.java
@@ -0,0 +1,10 @@
+package com.dooji.kurasu.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KurasuClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
